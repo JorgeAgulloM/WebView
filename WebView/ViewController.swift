92f40f48100a568632f8ae737fbbbf6f26e78759
JorgeAgulloM <agullojorge@gmail.com>
2022-03-10 10:04:07 +0100

    first commit

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, WKNavigationDelegate {
 
-class ViewController: UIViewController {
-
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://stackoverflow.com/")!
+        self.webView.load(URLRequest(url: url))
+
+        let refresBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self.webView, action: #selector(webView.reload))
+        self.toolbarItems = [refresBarButtonItem]
+        self.navigationController?.isToolbarHidden = false
+                          
     }
 
+    override func loadView() {
+        self.webView = WKWebView()
+        self.webView.navigationDelegate = self
+        self.view = self.webView
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation){
+        self.title = self.webView.title
+    }
 
 }
 
